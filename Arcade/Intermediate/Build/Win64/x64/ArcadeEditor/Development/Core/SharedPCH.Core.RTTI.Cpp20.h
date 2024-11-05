a20800738e495b448e7013c88c450f2ab6d80a97
KeceK <jfk.abrao@hotmail.com>
2024-11-05 15:41:33 -0300

    v11
    more save

--- Arcade/Intermediate/Build/Win64/x64/ArcadeEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/Users/jfkab/Documents/Unreal Projects/ArcadeSimulator/Arcade/Intermediate/Build/Win64/x64/ArcadeEditor/Development/Core/SharedDefinitions.Core.RTTI.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- Arcade/Intermediate/Build/Win64/x64/ArcadeEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/Users/jfkab/Documents/Unreal Projects/ArcadeSimulator/Arcade/Intermediate/Build/Win64/x64/ArcadeEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
