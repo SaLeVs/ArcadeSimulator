fc95518a310ffbae3bd34bb77941e9d0664dc981
João Felipe <jfk.abrao@hotmail.com>
2024-11-06 12:01:04 -0300

    v16
    start day system

---
+++ Arcade/Intermediate/Build/Win64/x64/ArcadeEditor/Development/Core/SharedPCH.Core.Cpp17.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.41.34123
+#include "F:/UnrealProjects/ArcadeSimulator/Arcade/Intermediate/Build/Win64/x64/ArcadeEditor/Development/Core/SharedPCH.Core.Cpp17.h"
---
+++ Arcade/Intermediate/Build/Win64/x64/ArcadeEditor/Development/Engine/SharedPCH.Engine.Cpp17.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "F:/UnrealProjects/ArcadeSimulator/Arcade/Intermediate/Build/Win64/x64/ArcadeEditor/Development/Engine/SharedDefinitions.Engine.Cpp17.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
