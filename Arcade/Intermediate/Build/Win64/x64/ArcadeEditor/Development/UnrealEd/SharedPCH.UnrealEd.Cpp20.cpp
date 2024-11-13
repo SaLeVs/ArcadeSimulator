d01c195888261217767faab8950e73a7d5d27b8a
SaLeVs <alvesanicetodasilvag@gmail.com>
2024-11-13 10:10:12 -0300

    v1
    New ui for machine stuck + animation to fade

--- Arcade/Intermediate/Build/Win64/x64/ArcadeEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.41.34123
-#include "C:/Users/jfkab/Documents/Unreal Projects/ArcadeSimulator/Arcade/Intermediate/Build/Win64/x64/ArcadeEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h"
