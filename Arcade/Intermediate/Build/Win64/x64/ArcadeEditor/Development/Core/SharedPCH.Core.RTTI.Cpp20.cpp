ed635d7a067e466ae60640e6a7aa6df3abb4ea91
TUTUJOOJ <spyrojorge@gmail.com>
2024-11-19 19:57:33 -0300

    V1
    Add new Npc Mesh Material Animation

---
+++ Arcade/Intermediate/Build/Win64/x64/ArcadeEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.41.34123
+#include "C:/Users/Jorge Santos/Desktop/ARCADESIMULATOR/ArcadeSimulator/Arcade/Intermediate/Build/Win64/x64/ArcadeEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h"
---
+++ Arcade/Intermediate/Build/Win64/x64/ArcadeEditor/Development/Engine/SharedPCH.Engine.Cpp17.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.41.34123
+#include "C:/Users/Jorge Santos/Desktop/ARCADESIMULATOR/ArcadeSimulator/Arcade/Intermediate/Build/Win64/x64/ArcadeEditor/Development/Engine/SharedPCH.Engine.Cpp17.h"
